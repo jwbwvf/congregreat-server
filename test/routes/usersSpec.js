8b803d576aa156958fd769b2f7ecd36fb5383ec4
jwbwvf <jwbwvf@gmail.com>
2018-03-19 23:55:11 -0500

    initial unit test setup

---
+++ test/routes/usersSpec.js
@@ -0,0 +1,17 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../../app')
+
+const expect = chai.expect
+
+chai.use(chaiHttp)
+
+describe('users routes', function() {
+  describe('GET /users', function() {
+    it('should return respond with a resource', async function() {
+      const response = await chai.request(app).get('/users')
+      expect(response.status).to.equal(200)
+      expect(response.body).to.equal('respond with a resource')
+    });
+  });
+});
