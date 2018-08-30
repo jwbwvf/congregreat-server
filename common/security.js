01d0edad955eaf2eb74431e0bbbb22fe81203bf4
jwbwvf <jwbwvf@gmail.com>
2018-08-30 18:33:43 -0500

    updated based on code review

--- common/security.js
+++ common/security.js
@@ -6,17 +6,11 @@ const iterations = 1000
 const size = 16
 const digest = 'SHA256'
 
-const generateSalt = () => {
-  return crypto.randomBytes(size).toString('hex')
-}
-const generateHash = (salt, password) => {
-  return crypto.pbkdf2Sync(password, salt, iterations, size, digest).toString('hex')
-}
+const generateSalt = () => crypto.randomBytes(size).toString('hex')
 
-const isPasswordValid = function (password, salt, hash) {
-  const generatedHash = crypto.pbkdf2Sync(password, salt, iterations, size, digest).toString('hex')
-  return hash === generatedHash
-}
+const generateHash = (salt, password) => crypto.pbkdf2Sync(password, salt, iterations, size, digest).toString('hex')
+
+const isPasswordValid = (password, salt, hash) => hash === crypto.pbkdf2Sync(password, salt, iterations, size, digest).toString('hex')
 
 module.exports = {
   generateSalt,
--- common/token.js
+++ common/token.js
@@ -8,26 +8,10 @@ const privateKey = fs.readFileSync(config.jwt.private)
 const passphrase = config.jwt.passphrase
 const algorithm = 'RS512'
 
-const verifyToken = token => {
-  return jsonWebToken.verify(
-    token,
-    publicKey,
-    { algorithm }
-  )
-}
+const verifyToken = token => jsonWebToken.verify(token, publicKey, { algorithm })
 
-const generateToken = (payload, expirationInDays = 2) => {
-  return jsonWebToken.sign(
-    payload,
-    {
-      key: privateKey,
-      passphrase
-    }, {
-      algorithm,
-      expiresIn: `${expirationInDays}d`
-    }
-  )
-}
+const generateToken = (payload, expirationInDays = 2) => jsonWebToken.sign(
+  payload, { key: privateKey, passphrase }, { algorithm, expiresIn: `${expirationInDays}d` })
 
 module.exports = {
   verifyToken,
