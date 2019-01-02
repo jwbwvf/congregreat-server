7b769c0daa78ffade708fc67219bb182f51d8c6a
jwbwvf <jwbwvf@gmail.com>
2019-01-01 21:40:18 -0600

    corrected member tests

--- routes/admin/members.js
+++ routes/admin/members.js
@@ -10,37 +10,11 @@ const {
   softDelete
 } = require('../../controllers/member')
 
-/**
- * Get all members across all congregations.
- */
 router.get('/', getAll)
-
-/**
- * Get a member by id.
- */
 router.get('/:id', getById)
-
-/**
- * Get all members of a congregation.
- */
 router.get('/congregations/:id', getByCongregationId)
-
-/**
- * Add a new member.
- * firstName, lastName, email, and congregationId are required.
- * Returns an error if the member already exists(email).
- */
 router.post('/', create)
-
-/**
- * Update a member by id.
- */
 router.patch('/:id', update)
-
-/**
- * Delete member by id.
- * Soft delete.
- */
 router.delete('/:id', softDelete)
 
 module.exports = router
